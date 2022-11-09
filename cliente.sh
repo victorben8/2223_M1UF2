ce54621cd629c856f16b19ce35b04a201ff091ed
enti <enti@enti.enti>
2022-11-09 11:12:14 +0100

    Comprobaciones y vacas

--- cliente.sh
+++ cliente.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
+PORT="3745"
+SERVER_AD="localhost"
+
 echo "Cliente TURIP"
 
 echo "(1)SEND MSG: HOLI_TURIP"
-echo "HOLI_TURIP 127.0.0.1" | nc localhost 2223
+
+echo "HOLI_TURIP 127.0.0.1" | nc $SERVER_AD $PORT
+
+echo "(2) Listen: comprobación del handshake"
+
+MSG=`nc -l $PORT`
+
+echo $MSG
+
+if [ $MSG != "OK_TURIP" ]
+then
+
+	echo "ERROR 1: Handshake incorrecto"
+	
+	exit 1
+fi
+
+echo "(5)SEND"
+echo "FILE_NAME vaca.vaca" | nc -l $PORT
+echo "(6)listen"
+
+
+exit 0
--- servidor.sh
+++ servidor.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+PORT="3745"
+
 echo "Servidor Transfer Unite Recursive International Protocol (TURIP)"
 
 echo "(0)LISTEN"
-MSG=`nc -l 2223`
 
-if [ "$MSG" != "HOLI_TURIP" ]
+MSG=`nc -l $PORT`
+
+HANDSHAKE=`echo $MSG | cut -d " " -f 1`
+IP_CLIENT=`echo $MSG | cut -d " " -f 2`
+
+echo "(3) comprobación del handshake"
+if [ "$HANDSHAKE" != "HOLI_TURIP" ]
 then
+	
 	echo "ERROR 1: Handshake incorrecto"
+
+	echo "KO_TURIP" | nc $IP_CLIENT $PORT
+	
 	exit 1
 fi
 
+echo "OK_TURIP" | nc $IP_CLIENT
+
+echo "(4) LISTEN"
+
+MSG=`nc -l $PORT`
+
+FILE_NAME=`echo $MSG | cut -d " " -f 2`
+
+
+
+exit 0
