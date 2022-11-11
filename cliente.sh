befd6a73a54da6b05dc71a60bf283a196ba2724d
enti <enti@enti.enti>
2022-11-11 11:16:23 +0100

    Hasta el paso 9 del protocolo TURIP

--- cliente.sh
+++ cliente.sh
@@ -1,31 +1,46 @@
 #!/bin/bash
 
-PORT="3745"
-SERVER_AD="localhost"
+PORT="2223"
+SERVER_ADDRESS="localhost"
 
 echo "Cliente TURIP"
 
-echo "(1)SEND MSG: HOLI_TURIP"
+echo "(1) SEND MSG: HOLI_TURIP 127.0.0.1"
+echo "HOLI_TURIP 127.0.0.1" | nc $SERVER_ADDRESS $PORT
 
-echo "HOLI_TURIP 127.0.0.1" | nc $SERVER_AD $PORT
+# Nos ponemos en escucha por el puerto 2223 para recibir la respuesta
 
-echo "(2) Listen: comprobación del handshake"
-
+echo "(2) LISTEN: Comprobacion Handshake"
 MSG=`nc -l $PORT`
 
-echo $MSG
+# Validamos si el mensaje recibido es OK_TURIP
+# En caso de recibir KO_TURIP, mostramos mensaje de error y salimos
+#	con el código de estado 1
 
-if [ $MSG != "OK_TURIP" ]
+if [ "$MSG" != "OK_TURIP" ]
 then
-
 	echo "ERROR 1: Handshake incorrecto"
-	
 	exit 1
 fi
 
-echo "(5)SEND"
-echo "FILE_NAME vaca.vaca" | nc -l $PORT
-echo "(6)listen"
+# Enviamos el nombre del archivo a generar por el servidor
+echo "(5) SEND MSG: FILE_NAME filename"
 
+FILE_NAME="vaca.vaca"
+
+echo "FILE_NAME $FILE_NAME" | nc $SERVER_ADDRESS $PORT
+
+echo "(6) LISTEN: Comprobacion Filename"
+MSG=`nc -l $PORT`
+
+if [ "$MSG" != "OK_FILE_NAME" ]
+then
+	echo "ERROR 2: Nombre de archivo mal formado"
+	exit 2
+fi
+
+echo "(9) SEND: Datos de vaca"
+cat vacas/$FILE_NAME | nc $SERVER_ADDRESS $PORT
 
 exit 0
+
--- servidor.sh
+++ servidor.sh
@@ -1,35 +1,56 @@
 #!/bin/bash
 
-PORT="3745"
+# Nombre del protocolo
+# Transfer Unite Recursive International Protocol
+# TURIP
+# Puerto 2223
 
-echo "Servidor Transfer Unite Recursive International Protocol (TURIP)"
+PORT="2223"
 
-echo "(0)LISTEN"
+echo "Servidor Transfer Unite Recursive International Protocol"
 
+echo "(0) LISTEN HANDSHAKE"
 MSG=`nc -l $PORT`
+
+# Revisamos si el mensaje recibido es correcto
+# Para ello separados el código de error recibido de la IP
+# En caso de no ser correcto, mostraremos un mensaje de ERROR
+# 	y salimos con el código de estado 1
 
 HANDSHAKE=`echo $MSG | cut -d " " -f 1`
 IP_CLIENT=`echo $MSG | cut -d " " -f 2`
 
-echo "(3) comprobación del handshake"
+# Revisamos todas las opciones que se encuentran mal y por
+# último hacemos la opción correcta fuera del IF
+echo "(3) SEND MSG: OK_TURIP / KO_TURIP"
 if [ "$HANDSHAKE" != "HOLI_TURIP" ]
 then
-	
 	echo "ERROR 1: Handshake incorrecto"
-
-	echo "KO_TURIP" | nc $IP_CLIENT $PORT
-	
+	echo "KO_TURIP" | nc $IP_CLIENT $PORT 
 	exit 1
 fi
 
-echo "OK_TURIP" | nc $IP_CLIENT
+echo "OK_TURIP" | nc $IP_CLIENT $PORT 
 
-echo "(4) LISTEN"
-
+echo "(4) LISTEN FILENAME"
 MSG=`nc -l $PORT`
 
-FILE_NAME=`echo $MSG | cut -d " " -f 2`
+# Cogemos el prefijo y el nombre de archivo
+PREFIX=`echo $MSG | cut -d " " -f 1`
+FILENAME=`echo $MSG | cut -d " " -f 2`
 
+echo "(7) SEND MSG: OK_FILE_NAME / KO_FILE_NAME"
+if [ "$PREFIX" != "FILE_NAME" ]
+then
+	echo "ERROR 2: Nombre de archivo mal formado"
+	echo "KO_FILE_NAME" | nc $IP_CLIENT $PORT
+	exit 2
+fi
 
+echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
 
+echo "(8) LISTEN: Datos de vaca"
+MSG=`nc -l $PORT`
+
+nc -l $PORT > inbox/$FILE_NAME
 exit 0
